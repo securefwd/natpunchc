8a84466daabcaf7f6f7cbeba65cf27e6be732f6c
blueBuffalo <blue@securefwd.io>
2016-03-02 19:54:20 +0100

    get config from disk

--- app.js
+++ app.js
@@ -3,27 +3,28 @@ var dgram = require('dgram');
 var exec = require('child_process').exec;
 var sendudp = require('./sendudp');
 var network = require('network');
+var fs = require('fs');
 
 var serverUrl = 'http://151.80.177.19:3000';
 var socket = io.connect(serverUrl);
 
-var srcip = '';//'172.18.0.2';
 var srcport = '1194';
 //var destip = '151.80.177.19';
 var data = new Buffer(''); //Don't change this phrase for natpunch to work.
-
+var config = fs.readFileSync('/persistant/natpunchc/config.json', 'utf8');
 
 network.get_private_ip(function(err, ip){
 	if (err) {
 		console.log(err);
 	}else {
-		srcip = ip;
+		var srcip = ip;
 		console.log('myIP: ' + srcip);
 
 		console.log('Starting socket.io client...');
 		socket.on('connect', function () {
 		    console.log("socket connected");
-		    socket.emit('register', { name: 'blue', email: 'blue@securefwd.io', api: 'abc', port: '12345' });
+		    //socket.emit('register', { email: 'blue@securefwd.io', api: 'abc', port: '12345' });
+		    socket.emit('register', config);
 		});
 
 		socket.on('udpfire', function(msg){
